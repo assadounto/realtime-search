ddac8b468e41fe244ebac606677923681428d331
“rich” <adukyerer@gmail.com>
2025-04-11 17:10:18 +0000

    Reorder imports and registration of SearchFormController in index.js

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,11 @@
 // Import and register all your controllers from the importmap via controllers/**/*_controller
 import { application } from "controllers/application"
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
-import SearchFormController from "./search_form_controller";
+
+
+import SearchFormController from "controllers/search_form_controller";
+
+
 application.register("search-form", SearchFormController);
+import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading";
+
+eagerLoadControllersFrom("controllers", application);
